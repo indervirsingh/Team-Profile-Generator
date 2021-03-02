fb878dff5ad959053019af28e514b54383718664
Indervir Singh <iindervirsingh@gmail.com>
2021-03-01 17:14:37 -0800

    typos

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // *Variables* ________________________________________________________________________________________________________________
 
+    // Libraries needed/imported
+    const inquirer = require('inquirer');
+    const fs = require('fs');
+
+    // Classes needed/imported
     const Employee = require('./lib/Employee');
     const Engineer = require('./lib/Engineer');
     const Intern = require('./lib/Intern');
