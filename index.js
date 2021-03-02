f264a7fd7d24b2a80d7525b5122c6e8e4878c418
Indervir Singh <iindervirsingh@gmail.com>
2021-03-01 17:51:05 -0800

    checking t see if data is being sent properly

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
     // Libraries needed/imported
     const inquirer = require('inquirer');
     const fs = require('fs');
+    const util = require('util');
 
     // Classes needed/imported
     const Employee = require('./lib/Employee');
@@ -18,22 +19,22 @@
     const addEmployee = (name, id, email) => {
         let employee = new Employee(name, id, email);
         return employee;
-    }
+    };
 
     // Create an Engineer type employee
     const createEngineer = ({name, id, email}, github) => {
         let engineer = new Engineer(name, id, email, github);
-    }
+    };
 
     // Create a Manager type employee
     const createManager = ({name, id, email}, officeNumber) => {
         let manager = new Engineer(name, id, email, officeNumber);
-    }
+    };
 
     // Create an Intern type employee
     const createIntern = ({name, id, email}, school) => {
         let manager = new Intern(name, id, email, school);
-    }
+    };
 
     // Create writeFile function using promises instead of a callback function
     const writeFileAsync = util.promisify(fs.writeFile);
@@ -56,10 +57,12 @@
                 message: 'Please enter your team manager\'s email address:'
             },
             {
-
+                type: 'input',
+                name: 'officeNumber',
+                message: 'Please enter your team manager\s office number:'
             }
         ]);
-    }
+    };
 
     // Initialize the app
     const init = () => {
@@ -67,16 +70,16 @@
         // Get team manager info first
         teamManagerPrompt()
             .then(answers => addEmployee(answers.managerName, answers.managerId, answers.managerEmail))
-            then((employee) => console.log('Team manager has been created: ' + employee))
+            .then((employee) => console.log('Team manager has been created: ' + JSON.stringify(employee)))
             .catch(error => console.error(error));
-    }
+    };
 
     // Generate the HTML page
     const generateHTML = (...employees) => {
         // Using the employees array, loop through and create a card for each employee
         // Maybe a switch statement depending on type of employee
 
-    }
+    };
 
 
 // ____________________________________________________________________________________________________________________________
